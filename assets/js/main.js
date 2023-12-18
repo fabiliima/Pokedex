34eca555436f0a2a3242a20b6c39cf8c0540e56d
fabiliima <fabilimaj@hotmail.com>
2023-12-18 22:15:10 +0000

    adiciona modal com informações extras do pokemon clicado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
+const modalDetails = document.getElementById('modal-details');
+const modal = document.getElementById('modal-id');
+
 const maxRecords = 151;
-const limit = 10;
+const limit = 3;
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
     const newHtml = pokemons.map((pokemon) => `
-            <li class="pokemon ${pokemon.type}">
+            <li id=${pokemon.number} class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
@@ -23,6 +26,51 @@ function loadPokemonItens(offset, limit) {
             </li>
     `).join('')
     pokemonList.innerHTML += newHtml
+
+        
+    const newPokemonItems = document.querySelectorAll('.pokemon');
+        newPokemonItems.forEach((item) => {
+            item.addEventListener('click', function(event) {
+                const liClicado = event.currentTarget;
+                const urlClicado = `https://pokeapi.co/api/v2/pokemon/${liClicado.id}`;
+                modal.style.display = "block";
+                fetch(urlClicado)
+                .then(response => response.json())
+                .then(dataPokemon => {
+                    const pokemonInfo = new PokemonDetails(
+                        dataPokemon.name,
+                        dataPokemon.height,
+                        dataPokemon.weight,
+                        dataPokemon.species.name,
+                        dataPokemon.sprites.front_default,
+                        
+                    )
+                    modalInfo(pokemonInfo);
+                    console.log(pokemonInfo)
+                })
+                fetch(urlClicado)
+                .then(response => response.json())
+                .then(extraDataPokemon => {
+                    
+                    const abilities = extraDataPokemon.abilities.map(ability => ability.ability.name)
+
+                    document.getElementById('pokemon-abilities').innerText = `Abilities: ${abilities}`
+
+                })
+    })
+
+    function modalInfo(pokemonInfo) {
+        document.getElementById('pokemon-name').innerText = `${pokemonInfo.name}`
+        document.getElementById('pokemon-height').innerText = `Height: ${pokemonInfo.height / 10} meters`
+        document.getElementById('pokemon-weight').innerText = `Weight: ${pokemonInfo.weight / 10} kilograms`
+        document.getElementById('pokemon-species').innerText = `Species: ${pokemonInfo.species}`
+
+        const modalphoto = document.getElementById('pokemon-photo')
+        modalphoto.src = pokemonInfo.photo
+        modalphoto.alt = pokemonInfo.name    
+    }
+
+})
     })
 }
  
@@ -42,5 +90,15 @@ loadMoreButton.addEventListener('click', () => {
     } else {
 
     loadPokemonItens(offset, limit)
+    }
+})
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+window.addEventListener('click', function(event) {
+    if(event.target === modal) {
+        closeModal()
     }
 })
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -5,3 +5,13 @@ class Pokemon {
     types = [];
     photo;
 }
+
+class PokemonDetails {
+    constructor(name, height, weight, species, photo) {
+    this.name = name;
+    this.height = height;
+    this.weight = weight;
+    this.species = species;
+    this.photo = photo;
+}
+}
